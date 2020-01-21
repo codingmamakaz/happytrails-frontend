5731cdd5d82e9aaafa1daef1da8e70f495590cad
Kazumi Karbowski <kazkazinus@gmail.com>
2020-01-21 00:16:36 -0500

    Refactor with Redux hooks

--- src/Components/reviews/TrailReviews.js
+++ src/Components/reviews/TrailReviews.js
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import TrailReview from './TrailReview'
 import { getMyReviews } from '../../actions/reviews'
 
-class TrailReviews extends Component {
+const TrailReviews = () => {
+  const reviews = useSelector(state => state.reviews)
+  const dispatch = useDispatch()
 
   //this is to make sure whenever a user refreshes a screen, the reviews are here again.
-  componentDidMount() {
-    this.props.getMyReviews()
-  }
+  useEffect(() => {
+    dispatch(getMyReviews())
+  })
 
-  render() {
-    let newlyCreatedReviewObj
-    let trailName
-    let trailUrl
+  let newlyCreatedReviewObj
+  let trailName
+  let trailUrl
 
-    newlyCreatedReviewObj = this.props.reviews.currentUserReviews[this.props.reviews.currentUserReviews.length - 1]
-    trailName = newlyCreatedReviewObj && newlyCreatedReviewObj.attributes.api_trail_name
-    trailUrl = newlyCreatedReviewObj && newlyCreatedReviewObj.attributes.api_trail_url
+  newlyCreatedReviewObj = reviews.currentUserReviews[reviews.currentUserReviews.length - 1]
+  trailName = newlyCreatedReviewObj && newlyCreatedReviewObj.attributes.api_trail_name
+  trailUrl = newlyCreatedReviewObj && newlyCreatedReviewObj.attributes.api_trail_url
 
-    const reviewArr = newlyCreatedReviewObj && newlyCreatedReviewObj.attributes.api_reviews.map(review => {
-      return (
-        <TrailReview
-          key={review.id}
-          review={review}
-        />
-      )
-    })
+  const reviewArr = newlyCreatedReviewObj && newlyCreatedReviewObj.attributes.api_reviews.map(review => {
     return (
-      <div className="has-background-white">
-        <div className="is-size-4 has-text-weight-bold"><a href={trailUrl} target="_blank" rel="noopener noreferrer"><span className="icon is-small"><i className="fas fa-link"></i></span> {trailName}</a>
-          {reviewArr.reverse()}
-        </div >
-      </div>
+      <TrailReview
+        key={review.id}
+        review={review}
+      />
     )
-  }
+  })
+  return (
+    <div className="has-background-white">
+      <div className="is-size-4 has-text-weight-bold"><a href={trailUrl} target="_blank" rel="noopener noreferrer"><span className="icon is-small"><i className="fas fa-link"></i></span> {trailName}</a>
+        {reviewArr.reverse()}
+      </div >
+    </div>
+  )
 }
-
-const mapStateToProps = state => ({ reviews: state.reviews })
-
-export default connect(mapStateToProps, { getMyReviews })(TrailReviews)
+export default TrailReviews
