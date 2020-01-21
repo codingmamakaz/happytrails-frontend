aff92bc80911250807e7c02acfc7ff74e2d576ca
Kazumi Karbowski <kazkazinus@gmail.com>
2020-01-20 23:44:04 -0500

    Refactor with Redux hooks

--- src/Components/reviews/MyReview.js
+++ src/Components/reviews/MyReview.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
+import { useDispatch } from "react-redux"
 import { deleteReview } from '../../actions/reviews'
-import { createBrowserHistory } from "history";
+import { createBrowserHistory } from "history"
 
-class MyReview extends Component {
+const MyReview = ({ review, review_id }) => {
+  const history = createBrowserHistory()
+  const dispatch = useDispatch()
 
-  render() {
-    const history = createBrowserHistory()
-
-    const { review } = this.props
-    return (
-      <>
-        <article className="message">
-          <div className="message-header has-background-white">
-            <div className="has-text-primary"><a href={review.api_trail_url} target="_blank" rel="noopener noreferrer"><span className="icon is-small"><i className="fas fa-link"></i></span> {review.api_trail_name}</a></div>
-            <div className="message-right">
-              <button onClick={() => { this.props.deleteReview(this.props.review_id, history); }} className="delete"></button>
-            </div>
+  return (
+    <>
+      <article className="message">
+        <div className="message-header has-background-white">
+          <div className="has-text-primary"><a href={review.api_trail_url} target="_blank" rel="noopener noreferrer"><span className="icon is-small"><i className="fas fa-link"></i></span> {review.api_trail_name}</a></div>
+          <div className="message-right">
+            <button onClick={() => dispatch(deleteReview(review_id, history))} className="delete"></button>
           </div>
-          <div className="message-body has-background-white">
-            {review.comment}
-          </div>
-        </article>
-      </>
-    )
-  }
+        </div>
+        <div className="message-body has-background-white">
+          {review.comment}
+        </div>
+      </article>
+    </>
+  )
 }
-
-export default connect(state => ({ reviews: state.reviews }), { deleteReview })(MyReview)
+export default MyReview
