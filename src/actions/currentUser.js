7bc2a2bd01ba5b760ccb2932a0d7855caf6a863b
Kazumi Karbowski <kazkazinus@gmail.com>
2020-02-10 23:52:15 -0500

    Trying to fix "undefined upon signing in

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -81,7 +81,10 @@ export const getCurrentUser = () => {
     })
       .then(res => res.json())
       .then(user => {
-        if (user.error !== "No one logged in") {
+        if (user.error !== "No one is logged in") {
+          //if the error message is "No one is logged in", which is coming from the back-end
+          //then ignore and move on. Otherwise alert the user.error
+          //This way, there isn't alert popping up upon every getCurrentUser().
           alert(user.error)
         } else if (user.data) {
           dispatch(setCurrentUser(user.data.attributes))
